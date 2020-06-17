a8c1a587a02c8a746f5cb877f0340fc8bc954fb0
ingduk2 <ingduk2@naver.com>
2020-06-17 21:53:50 +0900

    First Complete

--- App.js
+++ App.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert} from "react-native";
+import Loading from "./Loading";
+import * as Location from 'expo-location';
+import axios from 'axios';
+import Weather from './Weather';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}></View>
-      <View style={styles.blueView}></View>
-    </View>
-  );
+const API_KEY = "001fe92ed831b5e6bcfb132a53b2139c";
+
+export default class extends React.Component {
+state = {
+  isLoading : true
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },yellowView :{
-    flex:1,
-    backgroundColor: "yellow"
-  },
-  blueView:{
-    flex: 3,
-    backgroundColor: "blue"
+  getWeather = async(latitude, longitude) => {
+    const {
+      data : {
+        main :{temp},
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    
+    this.setState({
+      isLoading : false, 
+      temp,
+      condition:weather[0].main
+    });
+  }
+  getLocation = async() => {
+    try{
+      await Location.requestPermissionsAsync();
+      const {coords: {latitude, longitude}
+    } = await Location.getCurrentPositionAsync();
+    this.getWeather(latitude, longitude);
+    
+      //send to API and get Weather
+    }catch (error) {
+      Alert.alert("Can't find you.", "So Sad");
+    }
   }
 
-});
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const {isLoading, temp, condition} = this.state;
+    console.log(condition);
+    return isLoading ? <Loading/> : <Weather temp = {Math.round(temp)} condition={condition}/>;
+  }
+}
+
--- Loading.js
+++ Loading.js
@@ -1,19 +1,25 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , StatusBar} from 'react-native';
 
-export function Loading(){
+export default function Loading(){
     return (
         <View style={styles.container}>
-            <Text>Getting the fucking weather</Text>
+            <StatusBar barStyle="dark-content" />
+            <Text style={styles.text}>Getting the fucking weather</Text>
         </View>
     );
-}
+} 
 
 const styles = StyleSheet.create({
     container:{
         flex: 1,
         justifyContent: "flex-end",
         paddingHorizontal: 30,
-        paddingVertical: 100
+        paddingVertical: 100,
+        backgroundColor: "#FDF6AA"
+    },
+    text:{
+        color:"#2c2c2c",
+        fontSize: 30
     }
-});
+})
